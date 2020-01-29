13ce5f99a89c43228e6d773c64e3915cdad3fc4e
James Trinder <jamesanthonyt@live.co.uk>
2020-01-29 17:39:41 +0000

    Adding active storage. Error occurs when creating a new cocktail with a photo

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,4 +1,5 @@
 class Cocktail < ApplicationRecord
+  has_one_attached :photo
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses, dependent: :restrict_with_exception
   validates :name, presence: true, uniqueness: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_29_164851) do
+ActiveRecord::Schema.define(version: 2020_01_29_173420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
