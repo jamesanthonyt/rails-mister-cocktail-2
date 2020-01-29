1faf03e42b954ed4c92ae67a77d9b0f6b8b3dc0c
James Trinder <jamesanthonyt@live.co.uk>
2020-01-29 12:59:20 +0000

    Changed root to point to cockatils index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :cocktails, only: [:index, :show, :create, :new] do
+  root 'cocktails#index'
+
+  resources :cocktails, only: [:show, :create, :new] do
     resources :doses, only: [:new, :create, :destroy]
   end
   resources :doses, only: [:destroy]
