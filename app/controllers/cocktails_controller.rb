1f78748b1dfc45b8c53cd099bfd74747f2be2e6e
James Trinder <jamesanthonyt@live.co.uk>
2020-01-30 20:44:43 +0000

    Fixed some styling and the encryption error

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -18,7 +18,7 @@ class CocktailsController < ApplicationController
     if @cocktail.save
       redirect_to cocktail_path(@cocktail)
     else
-      render :edit
+      render :new
     end
   end
 
